9c5e2d2b8b6b996ab6416d737f7434b588f559e5
Yi Chen <sycn02430@gmail.com>
2022-10-20 00:35:49 +0800

    bad

--- src/main/resources/static/js/choose.js
+++ src/main/resources/static/js/choose.js
@@ -1,41 +1,41 @@
 $(document).ready(function() {
     var results = $('#result') ;
-//    $(function(){
-//        //console.log($(this).text());
-//        $.ajax({
-//            type: "GET",
-//            url: "http://localhost:8080/products",
-//            success: function(allProducts){
-//                results.html("");
-//                $.each(allProducts,function (i,product) {
-//
-//                        results.append(
-//                            "<div class='card' id = o_card>" +
-//                            "<div class='card-header'>" + product.id + "</div>" +
-//                            "<div class='card-body bg-dark text-white'>" +
-//                            "<p>" + "金額:" + product.price + "<br>" + "分類:" + product.category +"描述"+product.date+
-//                            "</p>" +
-//                            `<div class="card">
-//                                    <div class="card-header">
-//                                        <a class="btn" data-bs-toggle="collapse" href="#collapse${i}">
-//                                            詳細資訊
-//                                        </a>
-//                                    </div>
-//                                    <div id="collapse${i}" class="collapse" data-bs-parent="#accordion">
-//                                        <div class="card-body">
-//                                             <p style="color: black;">${product.description}</p>
-//                                        </div>
-//                                    </div>
-//                                </div>` +
-//
-//                            "</div>" +
-//                            "</div>"
-//                        )
-//
-//                });
-//            }
-//        });
-//    });
+    $(function(){
+        //console.log($(this).text());
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080/products",
+            success: function(allProducts){
+                results.html("");
+                $.each(allProducts,function (i,product) {
+
+                        results.append(
+                            "<div class='card' id = o_card>" +
+                            "<div class='card-header'>" + product.id + "</div>" +
+                            "<div class='card-body bg-dark text-white'>" +
+                            "<p>" + "金額:" + product.price + "<br>" + "分類:" + product.category +"描述"+product.date+
+                            "</p>" +
+                            `<div class="card">
+                                    <div class="card-header">
+                                        <a class="btn" data-bs-toggle="collapse" href="#collapse${i}">
+                                            詳細資訊
+                                        </a>
+                                    </div>
+                                    <div id="collapse${i}" class="collapse" data-bs-parent="#accordion">
+                                        <div class="card-body">
+                                             <p style="color: black;">${product.description}</p>
+                                        </div>
+                                    </div>
+                                </div>` +
+
+                            "</div>" +
+                            "</div>"
+                        )
+
+                });
+            }
+        });
+    });
     $("#btn1").click(function() {
         var data = {
             "date": "10/17",
@@ -54,9 +54,9 @@ $(document).ready(function() {
                 console.log(returnData);
                 alert("Post Success!!!");
             }
-            error: function(data) {
-                console.log("mission failed");
-            }
+//            error: function(data) {
+//                console.log("mission failed");
+//            }
         });
     });
 });
