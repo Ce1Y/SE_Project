ab505a0da34f1f3ae514391395e2d4536e7787f7
Yi Chen <sycn02430@gmail.com>
2022-10-20 01:38:02 +0800

    update read.js

--- src/main/resources/static/js/read.js
+++ src/main/resources/static/js/read.js
@@ -13,21 +13,23 @@ $(document).ready(function() {
                             "<div class='card' id = o_card>" +
                             "<div class='card-header'>" + product.id + "</div>" +
                             "<div class='card-body bg-dark text-white'>" +
-                            "<p>" + "金額:" + product.price + "<br>" + "分類:" + product.category +"描述"+product.date+
+                            "<p>" +
+                            "日期: " + product.date + "</br>" +
+                            "分類: " + product.category + "</br>" +
+                            "金額: " + product.price +
                             "</p>" +
                             `<div class="card">
-                                    <div class="card-header">
-                                        <a class="btn" data-bs-toggle="collapse" href="#collapse${i}">
-                                            詳細資訊
-                                        </a>
+                                <div class="card-header">
+                                    <a class="btn" data-bs-toggle="collapse" href="#collapse${i}">
+                                        詳細資訊
+                                    </a>
+                                </div>
+                                <div id="collapse${i}" class="collapse" data-bs-parent="#accordion">
+                                    <div class="card-body">
+                                        <p style="color: black;">${product.description}</p>
                                     </div>
-                                    <div id="collapse${i}" class="collapse" data-bs-parent="#accordion">
-                                        <div class="card-body">                                        
-                                             <p style="color: black;">${product.description}</p>
-                                        </div>
-                                    </div>
-                                </div>` +
-
+                                </div>
+                            </div>` +
                             "</div>" +
                             "</div>"
                         )
