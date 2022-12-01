23bc39a95f4cd4d460eadcbd51f7395d6439f12e
Eric <0524eric@gmail.com>
2022-12-01 22:52:15 +0800

    email

---
+++ src/main/java/SE_Project/demo/service/EmailService.java
@@ -0,0 +1,15 @@
+package SE_Project.demo.service;
+
+import SE_Project.demo.model.EmailDetails;
+
+// Interface
+public interface EmailService {
+
+    // Method
+    // To send a simple email
+    String sendSimpleMail(EmailDetails details);
+
+    // Method
+    // To send an email with attachment
+    String sendMailWithAttachment(EmailDetails details);
+}
